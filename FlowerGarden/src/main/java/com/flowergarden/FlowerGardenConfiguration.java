90e747021fcf23655a6c0f73c7ae35f8f0d13883
Andrey Bobrov <verydrybeaver@gmail.com>
2018-03-25 17:24:39 +0300

    - remove xml;

---
+++ FlowerGarden/src/main/java/com/flowergarden/FlowerGardenConfiguration.java
@@ -0,0 +1,31 @@
+package com.flowergarden;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class FlowerGardenConfiguration {
+
+    @Value( "${jdbc.url}" )
+    private String jdbcUrl;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+    propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(jdbcUrl);
+        return dataSource;
+    }
+}
