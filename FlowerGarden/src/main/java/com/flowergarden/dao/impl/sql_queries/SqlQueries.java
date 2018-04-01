88ebb7c4e6ce83b430a1b08851a21d7be6eb9950
Andrey Bobrov <verydrybeaver@gmail.com>
2018-04-01 12:01:43 +0300

    - refactor;

--- FlowerGarden/src/main/java/com/flowergarden/dao/impl/sql_queries/SqlQueries.java
+++ FlowerGarden/src/main/java/com/flowergarden/dao/impl/sql_queries/SqlQueries.java
@@ -4,35 +4,35 @@ public class SqlQueries {
 
     public final static String SELECT_FLOWER_JOIN_BOUQUET =
             "select" +
-            "  b.id bouquet_id," +
-            "  b.name bouquet_name," +
-            "  b.assemble_price," +
-            "  f.id flower_id," +
-            "  f.name flower_name," +
-            "  f.length length," +
-            "  f.freshness," +
-            "  f.price," +
-            "  f.petals," +
-            "  f.spike " +
+            " b.id bouquet_id," +
+            " b.name bouquet_name," +
+            " b.assemble_price," +
+            " f.id flower_id," +
+            " f.name flower_name," +
+            " f.length length," +
+            " f.freshness," +
+            " f.price," +
+            " f.petals," +
+            " f.spike " +
             "from flower f" +
-            "  left join bouquet b" +
-            "  on f.bouquet_id = b.id";
+            " left join bouquet b" +
+            " on f.bouquet_id = b.id";
 
     public final static String SELECT_BOUQUET_JOIN_FLOWER =
             "select" +
-            "  b.id bouquet_id," +
-            "  b.name bouquet_name," +
-            "  b.assemble_price," +
-            "  f.id flower_id," +
-            "  f.name flower_name," +
-            "  f.length length," +
-            "  f.freshness," +
-            "  f.price," +
-            "  f.petals," +
-            "  f.spike " +
+            " b.id bouquet_id," +
+            " b.name bouquet_name," +
+            " b.assemble_price," +
+            " f.id flower_id," +
+            " f.name flower_name," +
+            " f.length length," +
+            " f.freshness," +
+            " f.price," +
+            " f.petals," +
+            " f.spike " +
             "from bouquet b" +
-            "  left join flower f" +
-            "  on f.bouquet_id = b.id";
+            " left join flower f" +
+            " on f.bouquet_id = b.id";
 
 
 }
