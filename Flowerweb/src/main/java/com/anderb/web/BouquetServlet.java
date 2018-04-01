6d522cf79e97f5b722403e502b75cea1b5b78e77
Andrey Bobrov <verydrybeaver@gmail.com>
2018-04-01 22:33:48 +0300

    - change servlet

--- Flowerweb/src/main/java/com/anderb/web/BouquetServlet.java
+++ Flowerweb/src/main/java/com/anderb/web/BouquetServlet.java
@@ -1,8 +1,10 @@
 package com.anderb.web;
 
+import com.flowergarden.domain.bouquet.GeneralBouquet;
 import com.flowergarden.service.GeneralBouquetService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.servlet.ServletConfig;
@@ -10,9 +12,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@Component
 public class BouquetServlet extends HttpServlet {
     private static Logger log = Logger.getLogger(BouquetServlet.class);
 
@@ -26,11 +30,17 @@ public class BouquetServlet extends HttpServlet {
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-        log.debug("");
-        PrintWriter writer = resp.getWriter();
-        writer.println("Bouquet 1: " + bouquetService.findOne(1));
-        writer.close();
+        response.setContentType("text/html");
+
+        String pathInfo = request.getPathInfo();
+        int id = Integer.parseInt(pathInfo.split("/")[1]);
+        GeneralBouquet bouquet = bouquetService.findOne(id);
+
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Bouquet " + id + ": " + bouquet + "</h2>");
+        out.close();
     }
 }
